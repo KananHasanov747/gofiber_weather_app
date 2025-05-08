a90b9b0f3432dec055df650b8db2122ef3b96bf1
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-05-08 16:25:43 +0400

    feat(app/models/models): update models

--- app/models/models.go
+++ app/models/models.go
@@ -1,5 +1,11 @@
 package models
 
+type Location struct {
+	City     string `json:"city"`
+	Country  string `json:"country"`
+	Location string `josn:"loc"`
+}
+
 type Search struct {
 	Name       string  `json:"name"`
 	Country    string  `json:"country"`
@@ -12,12 +18,23 @@ type SearchResponse struct {
 	Results []Search `json:"results"`
 }
 
+type Current struct {
+	Temperature_2m       float32 `json:"temperature_2m"`
+	Apparent_temperature float32 `json:"apparent_temperature"`
+	Icon_url             string  `json:"icon_url"`
+	Description          string  `json:"description"`
+	Rain                 float32 `json:"rain"`
+	Wind_speed_10m       float32 `josn:"wind_speed_10m"`
+}
+
 type Hourly struct {
 	Apparent_temperature      []float32 `json:"apparent_temperature"`
+	Icon_url                  []string  `json:"icon_url"`
+	Description               []string  `json:"description"`
 	Precipitation_probability []int     `json:"precipitation_probability"`
-	Rain                      []float32 `json:"rain"`
-	Relative_humidity_2m      []int     `json:"relative_humidity_2m"`
 	Temperature_2m            []float32 `json:"temperature_2m"`
+	Relative_humidity_2m      []int     `json:"relative_humidity_2m"`
+	Rain                      []float32 `json:"rain"`
 	Time                      []string  `json:"time"`
 	Weather_code              []int     `json:"weather_code"`
 	Wind_speed_10m            []float32 `json:"wind_speed_10m"`
@@ -26,7 +43,11 @@ type Hourly struct {
 type Daily struct {
 	Temperature_2m_max []float32 `json:"temperature_2m_max"`
 	Temperature_2m_min []float32 `json:"temperature_2m_min"`
+	Day_of_week        []string  `json:"day_of_week"`
 	Time               []string  `json:"time"`
+	Is_day             []int     `json:"is_day"`
+	Icon_url           []string  `json:"icon_url"`
+	Desription         []string  `json:"description"`
 	Uv_index_max       []float32 `json:"uv_index_max"`
 	Weather_code       []int     `json:"weather_code"`
 }
@@ -36,7 +57,8 @@ type WeatherResponse struct {
 	Country   string  `json:"country"`
 	Latitude  float32 `json:"latitude"`
 	Longitude float32 `json:"longitude"`
+	Timezone  string  `json:"timezone"`
+	Current   Current `json:"current"`
 	Hourly    Hourly  `json:"hourly"`
 	Daily     Daily   `json:"daily"`
-	Timezone  string  `json:"timezone"`
 }
